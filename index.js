f3976b905aa37d504953fc44670b6bbe7eb0adef
Leonardo Recalde <leo03recalde@gmail.com>
2024-09-26 16:13:26 -0300

    no se ejecuta el comando node --env-file .env --watch index.js

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 import express from 'express'
 
-console.log('hola mundo')
 //1-configurar un puerto, debe ser un repo q trabaje 24/7
+const app = express();
+
+app.set('puerto', process.env.PORT || 4000)
+app.listen(app.get('puerto')),
+() =>{
+    console.info('estoy escuchando el puerto'+ app.get('puerto'))
+}
+
 
 //2- configurar middlewares
 
