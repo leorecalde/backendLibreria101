f79f139e2bb3bbaf53687628f99a3f5fcf4ed8e9
Leonardo Recalde <leo03recalde@gmail.com>
2024-10-01 16:07:15 -0300

    se agraga conexion bd

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 import express from 'express'
+import morgan from 'morgan';
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import './src/database/dbConnection.js'
 
 //1-configurar un puerto, debe ser un repo q trabaje 24/7
 const app = express();
 
 app.set('puerto', process.env.PORT || 4000)
-app.listen(app.get('puerto')),
+app.listen(app.get('puerto'),
 () =>{
     console.info('estoy escuchando el puerto'+ app.get('puerto'))
 }
-
+)
 
 //2- configurar middlewares
 
+app.use(cors()) //permite otras conexiones REMOTAS
+app.use(morgan('dev')); // nos da info extra en la terminal
+app.use(express.json()); // interpreta los datos en formato json de la solicitud
+app.use(express.urlencoded({extended:true}))
+
+const __filename = fileURLToPath(import.meta.url);
+console.log(__filename)
+const __dirname = path.dirname(__filename)
+console.log(__dirname)
+console.log(path.join(__dirname, '/public'))
+
+app.use(express.static(path.join(__dirname, '/public')))
+
 //3- configurar las rutas
+// http://localhost:4000/prueba
+app.get('/prueba',(req, res) =>{
+    console.log('alguien hizo una solicitud get a la ruta de prueba')
+    res.send('hola mundo desde el backend')
+})
